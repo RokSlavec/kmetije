a565c7bcbfe6c1972eaa835bfc55150ab0deea60
Rok Slavec <rok.slavec@hotmail.com>
2015-04-03 12:15:55 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kmetije::Application.config.secret_token = '14bf70c477864970384e243b9c57c0e4bf687c5a508576d180e9446cc743930bfc0529e14692ffe20145e6cca3370d47dece6ce6fd254914a4906425018f7970'
