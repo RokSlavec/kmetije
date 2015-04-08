9feb6650fa28d1540860b0c33909a174046cea91
Rok Slavec <rok.slavec@hotmail.com>
2015-04-08 23:07:47 +0200

    Second commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def home
+	@title = "Slovenske kmetije"
   end
 
   def manage
+	@title = "Slovenske kmetije/mange"
   end
   
   def info
+	@title = "Slovenske kmetije/info"
   end
 end
