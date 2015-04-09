da6d7b5b1731f2d9a76065752798f4cb9bf8785a
Rok Slavec <rok.slavec@hotmail.com>
2015-04-09 11:08:58 +0200

    CSS and JS

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,10 @@
 class PagesController < ApplicationController
   def home
-	@title = "Slovenske kmetije"
   end
 
   def manage
-	@title = "Slovenske kmetije/mange"
   end
   
   def info
-	@title = "Slovenske kmetije/info"
   end
 end
